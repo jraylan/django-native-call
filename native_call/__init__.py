380d4356f4516225731d72b6bf8c8fe2c47d5e20
jraylan <jeffersonraylan@gmail.com>
2022-02-27 23:28:50 -0300

    Ajustes.

--- native_call/__init__.py
+++ native_call/__init__.py
@@ -1,21 +1,10 @@
 from .registry import registry
 from . import (
-    templatetags,
     apps,
-    constants,
-    exceptions,
     registry,
-    utils,
-    wrapper
 )
 
 __all__ = [
-    'registry',
-    'templatetags',
     'apps',
-    'constants',
-    'exceptions',
-    'registry',
-    'utils',
-    'wrapper'
+    'registry'
 ]
