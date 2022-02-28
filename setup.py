8e8b2f0f53827cd67109d31c58627a054124ef03
jraylan <jeffersonraylan@gmail.com>
2022-02-27 22:59:46 -0300

    Ajustes.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='django-native-call',
-    version='0.1.0',
+    version='0.1.1',
     packages=['native_call', 'native_call.migrations'],
     url='',
     license='apache',
