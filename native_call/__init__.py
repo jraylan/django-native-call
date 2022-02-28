7863bbfe67e13d36b85c963a5796a16de4a92ceb
jraylan <jeffersonraylan@gmail.com>
2022-02-27 23:35:52 -0300

    Ajustes.

--- native_call/__init__.py
+++ native_call/__init__.py
@@ -1,18 +1,5 @@
 from .registry import registry
-from . import (
-    templatetags,
-    constants,
-    exceptions,
-    registry,
-    utils,
-    wrapper
-)
 
 __all__ = [
-    'templatetags',
-    'constants',
-    'exceptions',
-    'registry',
-    'utils',
-    'wrapper'
+    'registry'
 ]
--- native_call/apps.py
+++ native_call/apps.py
@@ -4,3 +4,6 @@ from django.apps import AppConfig
 class DjangoNativeCallConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'native_call'
+
+    def ready(self):
+        from templatetags import native_call
